e17652e85890a8d380c2efd8b583b7e84c3e3398
heyviehuang <heyviehuang@gmail.com>
2022-03-21 16:29:29 +0800

    不含裝飾線、RWD、Swiper、hover 其餘切板完成

--- js/all.js
+++ js/all.js
@@ -1,10 +1,4 @@
 $(document).ready(function() {
-    $('.navCourse').click(function(event) {
-        event.preventDefault();
-        // $('.navCourse').toggleClass('active');
-        $('.navCourseList').stop().slideToggle(300);
-    });
-
     $('.btnTop').click(function(event) {
         event.preventDefault();
         $('html,body').animate({
@@ -12,34 +6,32 @@ $(document).ready(function() {
         }, 700);
     });
 
-    // var swiper = new Swiper('.banner-swiper', {
-    //     spaceBetween: 300,
-    //     centeredSlides: true,
-    //     autoplay: {
-    //         delay: 3000,
-    //         disableOnInteraction: false,
-    //     },
-    //     pagination: {
-    //         el: '.swiper-pagination',
-    //         clickable: true,
-    //     },
-    // });
+    $('#bookNow-date-checkIn, #bookNow-date-checkOut').datepicker();
+    // $('#bookNow-date-numOfPeople').selectmenu();
 
-    // var swiper = new Swiper('.index-view-swiper', {
-    //     spaceBetween: 20,
-    //     centeredSlides: true,
-    //     autoplay: {
-    //         delay: 3000,
-    //         disableOnInteraction: false,
-    //     },
-    //     pagination: {
-    //         el: '.swiper-pagination',
-    //         clickable: true,
-    //     },
-    // });
+    var swiper = new Swiper('.banner-swiper', {
+        spaceBetween: 300,
+        centeredSlides: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 
-    lightbox.option({
-        'resizeDuration': 200,
-        'wrapAround': true
-    })
+    var swiper = new Swiper('.index-view-swiper', {
+        spaceBetween: 20,
+        centeredSlides: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
 });
