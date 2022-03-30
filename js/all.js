bad4139ee4d94ed7277f6aa1008b2165e548e8ef
heyviehuang <heyviehuang@gmail.com>
2022-03-31 00:43:20 +0800

    -修改head內容 -修正Swiper -修改按鈕形狀 -新增裝飾線 -新增hover效果 -修改架構為最大1200px

--- js/all.js
+++ js/all.js
@@ -7,31 +7,23 @@ $(document).ready(function() {
     });
 
     $('#bookNow-date-checkIn, #bookNow-date-checkOut').datepicker();
-    // $('#bookNow-date-numOfPeople').selectmenu();
 
-    var swiper = new Swiper('.banner-swiper', {
-        spaceBetween: 300,
-        centeredSlides: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
+    var swiper = new Swiper(".index-viewSwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        freeMode: true,
+        // pagination: {
+        //     el: ".swiper-pagination",
+        //     clickable: true,
+        // },
     });
-
-    var swiper = new Swiper('.index-view-swiper', {
+    var swiper = new Swiper(".roomDetails-roomPicSwiper", {
+        slidesPerView: 3,
         spaceBetween: 20,
-        centeredSlides: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
+        freeMode: true,
+        // pagination: {
+        //     el: ".swiper-pagination",
+        //     clickable: true,
+        // },
     });
 });
