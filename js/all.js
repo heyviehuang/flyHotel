01b080073153f7b13b0c92d789acf66b1feeef35
heyviehuang <heyviehuang@gmail.com>
2022-04-03 00:38:46 +0800

    -index RWD修正 -新增index圖片標題 -微調動畫延遲視差

--- js/all.js
+++ js/all.js
@@ -1,22 +1,37 @@
 $(document).ready(function() {
-    $('.btnTop').click(function(event) {
-        event.preventDefault();
-        $('html,body').animate({
-            scrollTop: 0
-        }, 700);
-    });
-
-    $('#bookNow-date-checkIn, #bookNow-date-checkOut').datepicker();
-
-    var swiper = new Swiper(".index-viewSwiper", {
-        slidesPerView: 3,
+    var swiper = new Swiper(".bannerSwiper", {
         spaceBetween: 30,
-        freeMode: true,
-        // pagination: {
-        //     el: ".swiper-pagination",
-        //     clickable: true,
-        // },
+        centeredSlides: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
     });
+
+    var mm = window.matchMedia("(max-width: 600px)");
+    mm.addListener(resizeWidth);
+    resizeWidth(mm);
+
+    function resizeWidth(pMatchMedia) {
+        if (pMatchMedia.matches) {
+            var swiper = new Swiper(".index-viewSwiper", {
+                slidesPerView: 1,
+                spaceBetween: 30,
+                freeMode: true,
+            });
+        } else {
+            var swiper = new Swiper(".index-viewSwiper", {
+                slidesPerView: 3,
+                spaceBetween: 30,
+                freeMode: true,
+            });
+        }
+    }
+
     var swiper = new Swiper(".roomDetails-roomPicSwiper", {
         slidesPerView: 3,
         spaceBetween: 20,
@@ -26,6 +41,7 @@ $(document).ready(function() {
         //     clickable: true,
         // },
     });
+
 
     $(window).scroll(function() {
         var scrollPos = $(window).scrollTop();
@@ -39,5 +55,12 @@ $(document).ready(function() {
             }
         });
     });
+    $('.btnTop').click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+    $('#bookNow-date-checkIn, #bookNow-date-checkOut').datepicker();
 
 });
